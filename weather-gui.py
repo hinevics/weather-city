824fc729d67276ac7557071e3c9898c11d5dfe4b
Sasha Khinevich <hinevics@gmail.com>
2021-10-18 20:41:39 +0300

    creat gui

--- weather-gui.py
+++ weather-gui.py
@@ -2,23 +2,58 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import random
-st.title('Hello word')
 
-data1 = [random.random() for i in range(10)]
-data2 = [random.random() for i in range(10)]
+DEFAULT_CITY_NAME = 'London'
 
-option = st.sidebar.selectbox(
-    'Which number do you like best?',
-     ['City1', 'City2'])
+dataCurrent1 = [1, 2, 3, 4]
+dataHistorycal1 = [4, 3, 2, 1]
+
+class WeatherAPI:
+    @classmethod
+    def get_current(cls, city:str):
+        if city == 'London':
+            return [(0.8**i)**i for i in dataCurrent1]
+        elif city == 'Minsk':
+            return [10**(-i) for i in dataCurrent1]
+    
+    @classmethod
+    def get_historycal(cls, city:str):
+        if city == 'London':
+            return [2**i for i in dataCurrent1]
+        elif city == 'Minsk':
+            return [i**10 for i in dataCurrent1]
+    
+    @classmethod
+    def get_forecast(cls, city:str):
+        if city == 'London':
+            return [(-1022)**i for i in dataCurrent1]
+        elif city == 'Minsk':
+            return [10**i for i in dataCurrent1]
 
 dict_test = {
-    'City1': data1,
-    'City2': data2
-}
+    'Current':lambda city: WeatherAPI.get_current(city=city), 
+    'Historycal': lambda city: WeatherAPI.get_historycal(city=city),
+    'Forecast': lambda city: WeatherAPI.get_forecast(city=city),
+    }
 
+# dict_test = {
+#     'Current':WeatherAPI.get_current(city=option), 
+#     'Historycal': WeatherAPI.get_historycal(city=option),
+#     'Forecast': WeatherAPI.get_forecast(city=option),
+#     }
+
+option = st.sidebar.text_input(label='Write the city whose data you want to see:', value=DEFAULT_CITY_NAME)
+
+st.title('Weather data')
 'You selected:', option
 
-st.line_chart(data=dict_test[option])
+state = st.selectbox('How would you like to be contacted?',
+                      ('Current', 'Historycal', 'Forecast'))
+
+st.write('You selected:', state)
+
+st.line_chart(data=dict_test[state](city=option))
 col1, col2 = st.columns(2)
-col1.metric(label='Mode', value='{}'.format(np.mean(dict_test[option])))
-col2.metric(label='Max', value='{}'.format(np.max(dict_test[option])))
+col1.metric(label='Mode', value='{}'.format(np.mean(dict_test[state](city=option))))
+col2.metric(label='Max', value='{}'.format(np.max(dict_test[state](city=option))))
+
