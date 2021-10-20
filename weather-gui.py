a1d66f2368a0a254782708f16a06cd8397447b54
Sasha Khinevich <hinevics@gmail.com>
2021-10-20 10:38:12 +0300

    new gui lint

--- weather-gui.py
+++ weather-gui.py
@@ -1,3 +1,5 @@
+from os import name
+from pandas.core import api
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -5,34 +7,42 @@ import random
 import WeatherAPI as wa
 DEFAULT_CITY_NAME = 'London'
 
-dataCurrent1 = [1, 2, 3, 4]
-dataHistorycal1 = [4, 3, 2, 1]
+# если api_key не введен, то выводится тект о том что api не доступно
+api_key = st.sidebar.text_input(label='Enter the api key:', value='')
 
-dict_test = {
-    'Current':lambda city, api_key: wa.get_current(city=city, api_key=api_key), 
-    'Historycal': lambda city, api_key: wa.get_historycal(city=city, api_key=api_key),
-    'Forecast': lambda city, api_key: wa.get_forecast(city=city, api_key=api_key),
-    }
+def current(state:str, city:str, api_key:str):
+    data_request = wa.get_current(city=city, api_key=api_key)
+    st.write('You selected:', state)
+    # st.line_chart(data=dict_test[state](city=option))
+    col1, col2 = st.columns(2)
+    col1.metric(label='temp', value='{}'.format(np.mean(data_request['temp'])))
+    col2.metric(label='pressure', value='{}'.format(np.max(data_request['pressure'])))
+    st.image(image=data_request['weather_icon'])
 
-# dict_test = {
-#     'Current':WeatherAPI.get_current(city=option), 
-#     'Historycal': WeatherAPI.get_historycal(city=option),
-#     'Forecast': WeatherAPI.get_forecast(city=option),
-#     }
+def historycal(city:str, api_key:str):
+    return wa.get_current(city=city, api_key=api_key)
 
-option = st.sidebar.text_input(label='Write the city whose data you want to see:', value=DEFAULT_CITY_NAME)
-api_key = st.sidebar.text_input(label='Enter the api key:')
 
-st.title('Weather data')
-'You selected:', option
+def forecast(city:str, api_key:str):
+    return wa.get_current(city=city, api_key=api_key)
 
-state = st.selectbox('What kind of data do you want to see??',
-                      ('Current', 'Historycal', 'Forecast'))
 
-st.write('You selected:', state)
-dt = dict_test[state](city=option, api_key=api_key)
-# st.line_chart(data=dict_test[state](city=option))
-col1, col2 = st.columns(2)
-col1.metric(label='temp', value='{}'.format(np.mean(dt['temp'])))
-col2.metric(label='pressure', value='{}'.format(np.max(dt['pressure'])))
-st.image(image=dt['weather_icon'])
+if api_key == '':
+    # Добавить описание того что это за app'ка
+    st.text('Enter api key')
+else:
+    input_city = st.sidebar.text_input(label='Write the city whose data you want to see:', value=DEFAULT_CITY_NAME)
+
+    st.title('Weather data')
+    'You selected:', input_city
+
+    state = st.selectbox('What kind of data do you want to see??',
+                        ('Current', 'Historycal', 'Forecast'))
+    if state == "Current":
+        current(state=state, city=input_city, api_key=api_key)
+    elif state == 'historycal':
+        historycal(city=input_city, api_key=api_key)
+    elif state == "Forecast":
+        forecast(city=input_city, api_key=api_key)
+    
+   
