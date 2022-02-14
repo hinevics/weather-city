9a4dbbc0c026e2d92d3630c3262825fa0550676f
Sasha Khinevich <hinevics@gmail.com>
2022-02-14 12:46:51 +0300

    fix modul

--- weather_flask_app/flaskapp/src/graph_generator.py
+++ weather_flask_app/flaskapp/src/graph_generator.py
@@ -1,48 +1,52 @@
-import plotly
-import plotly.graph_objects as go
-# import plotly.express as px
-from plotly.subplots import make_subplots
+# #! This module will be developed later
+# To draw all possible graphs
+
+# from turtle import xcor
+# import plotly
+# import plotly.graph_objects as go
+# # import plotly.express as px
+# from plotly.subplots import make_subplots
 
 
-def multiple_axes(x, y1, y2):
-    fig = make_subplots(specs=[[{"secondary_y": True}]])
-    # fig = go.Figure()
+# def multiple_axes(x, y1, y2):
+#     fig = make_subplots(specs=[[{"secondary_y": True}]])
+#     # fig = go.Figure()
 
-    # max_temps
-    fig.add_trace(
-        go.Scatter(x=datetimes, y=max_temps, name="max_temp"),
-        secondary_y=False,)
-    # min_temps
-    fig.add_trace(
-        go.Scatter(x=datetimes, y=min_temps, name="min_temp"),
-        secondary_y=False,)
-    # snows
-    fig.add_trace(
-        go.Scatter(x=datetimes, y=snows, name="snow"),
-        secondary_y=True,)
+#     # max_temps
+#     fig.add_trace(
+#         go.Scatter(x=x, y=max_temps, name="max_temp"),
+#         secondary_y=False,)
+#     # min_temps
+#     fig.add_trace(
+#         go.Scatter(x=x, y=min_temps, name="min_temp"),
+#         secondary_y=False,)
+#     # snows
+#     fig.add_trace(
+#         go.Scatter(x=x, y=snows, name="snow"),
+#         secondary_y=True,)
 
-    # Add figure title
-    fig.update_layout(
-        title_text="Changes in snow thickness and changes in max/min temperature")
+#     # Add figure title
+#     fig.update_layout(
+#         title_text="Changes in snow thickness and changes in max/min temperature")
 
-# Set x-axis title
-    fig.update_xaxes(title_text="date")
+# # Set x-axis title
+#     fig.update_xaxes(title_text="date")
 
-# Set y-axes titles
-    fig.update_yaxes(title_text="<b>Temperature</b>, Celcius", secondary_y=False)
-    fig.update_yaxes(title_text="<b>Accumulated snowfall</b>, mm", secondary_y=True)
-    # fig.add_trace(go.Scatter(x=datetimes, y=max_temps,
-    #                          mode='lines',
-    #                          name='max_temps'))
-    # fig.add_trace(go.Scatter(x=datetimes, y=min_temps,
-    #                          mode='lines',
-    #                          name='min_temps'))
-    # fig.add_trace(go.Scatter(x=datetimes, y=snows,
-    #                          mode='lines',
-    #                          name='snows'))
-    # fig.add_trace(go.Scatter())
-    # fig = px.line(df, x="date", y="lifeExp", color="continent",
-    #               line_group="country", hover_name="country",
-    #               line_shape="spline", render_mode="svg")
+# # Set y-axes titles
+#     fig.update_yaxes(title_text="<b>Temperature</b>, Celcius", secondary_y=False)
+#     fig.update_yaxes(title_text="<b>Accumulated snowfall</b>, mm", secondary_y=True)
+#     # fig.add_trace(go.Scatter(x=datetimes, y=max_temps,
+#     #                          mode='lines',
+#     #                          name='max_temps'))
+#     # fig.add_trace(go.Scatter(x=datetimes, y=min_temps,
+#     #                          mode='lines',
+#     #                          name='min_temps'))
+#     # fig.add_trace(go.Scatter(x=datetimes, y=snows,
+#     #                          mode='lines',
+#     #                          name='snows'))
+#     # fig.add_trace(go.Scatter())
+#     # fig = px.line(df, x="date", y="lifeExp", color="continent",
+#     #               line_group="country", hover_name="country",
+#     #               line_shape="spline", render_mode="svg")
 
-    return fig
+#     return fig
